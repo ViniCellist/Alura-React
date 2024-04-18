69a038c972ef56cba5b2ecbf7f1530a2b1e37d95
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-04-18 12:41:41 -0300

    feat: cards

--- organo/src/App.js
+++ organo/src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Banner from './components/Banner/Banner';
 import Form from './components/Form/Form';
+import Time from './components/Time/Time';
 
 function App() {
 
@@ -15,6 +16,9 @@ function App() {
     <div className="App">
       <Banner />
       <Form aoColaboradorCadastro={colaborador => aoNovoColaboradorAdicionado(colaborador)} />
+      <Time nome="Programação"/>
+      <Time nome="Front-End"/>
+      <Time nome="Data Science"/>
     </div>
   );
 }
---
+++ organo/src/components/Time/Time.js
@@ -0,0 +1,12 @@
+import './Time.css';
+
+function Time(props) {
+
+    return(
+        <section className="time-sec">
+            <h3>{props.nome}</h3>
+        </section>
+    )
+}
+
+export default Time;
